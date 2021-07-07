ad5dd8c8d543eccb4231369a5e3d7831974e008f
vincue <vineet@cuebook.ai>
2021-07-07 14:34:16 +0530

    Changed option background and exception handled runQuery

--- api/anomaly/services/querys.py
+++ api/anomaly/services/querys.py
@@ -3,14 +3,19 @@ from access.data import Data
 
 
 class Querys:
-	"""
-	Services for querying connection
-	"""
+    """
+    Services for querying connection
+    """
 
-	@staticmethod
-	def runQuery(connectionType, connectionParams, query, limit=True):
-		res = ApiResponse("Error in getting data")
-		dataframe = Data.runQueryOnConnection(connectionType, connectionParams, query, limit)
-		data = dataframe.to_dict("records")
-		res.update(True, "Successfully retrieved data", data)
-		return res
+    @staticmethod
+    def runQuery(connectionType, connectionParams, query, limit=True):
+        res = ApiResponse("Error in getting data")
+        try:
+            dataframe = Data.runQueryOnConnection(
+                connectionType, connectionParams, query, limit
+            )
+            data = dataframe.to_dict("records")
+            res.update(True, "Successfully retrieved data", data)
+        except Exception as ex:
+            pass
+        return res
