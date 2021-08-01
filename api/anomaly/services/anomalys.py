899a546439c2eb9a237a4089a41606fe35e55042
vincue <vineet@cuebook.ai>
2021-08-02 04:15:28 +0530

    CUEOBSERVE #46 Removed services from ops/tasks

--- api/anomaly/services/anomalys.py
+++ api/anomaly/services/anomalys.py
@@ -1,12 +1,19 @@
+import json
+import traceback
+import datetime as dt
+import dateutil.parser as dp
 from django.template import Template, Context
-from anomaly.services import anomalyDefinitions
 from utils.apiResponse import ApiResponse
+from django.db.models import Q
 from anomaly.models import Anomaly, AnomalyCardTemplate
 from anomaly.serializers import AnomalySerializer
-from django.db.models import Q
+
+from ops.anomalyDetection import detect, dataFrameEmpty
+
 
 ANOMALY_DAILY_TEMPLATE = "Anomaly Daily Template"
-ANOMALY_HOURLY_TEMPLATE= "Anomaly Hourly Template"
+ANOMALY_HOURLY_TEMPLATE = "Anomaly Hourly Template"
+
 
 class Anomalys:
     """
@@ -14,22 +21,32 @@ class Anomalys:
     """
 
     @staticmethod
-    def getAnomalys(publishedOnly: bool=False,offset: int=0, limit:int = 50, searchQuery: str=None, sorter: dict = {}):
+    def getAnomalys(
+        publishedOnly: bool = False,
+        offset: int = 0,
+        limit: int = 50,
+        searchQuery: str = None,
+        sorter: dict = {},
+    ):
         """
         Gets anomalys
         """
         res = ApiResponse("Error in getting anomalies")
-        anomaliesObj = Anomaly.objects.filter(published=True) if publishedOnly else Anomaly.objects.all()
+        anomaliesObj = (
+            Anomaly.objects.filter(published=True)
+            if publishedOnly
+            else Anomaly.objects.all()
+        )
         count = anomaliesObj.count()
 
         if searchQuery:
-            anomaliesObj = Anomalys.searchOnAnomalys(anomaliesObj, searchQuery)
+            anomaliesObj = Anomalys.__searchOnAnomalys(anomaliesObj, searchQuery)
             count = anomaliesObj.count()
         if sorter.get("order", False):
             anomaliesObj = Anomalys.sortOnAnomalys(anomaliesObj, sorter)
-        anomaliesObj = anomaliesObj[offset:offset+limit]
+        anomaliesObj = anomaliesObj[offset : offset + limit]
         anomalies = AnomalySerializer(anomaliesObj, many=True).data
-        data = {"anomalies":anomalies , "count":count}
+        data = {"anomalies": anomalies, "count": count}
         res.update(True, "Successfully retrieved anomalies", data)
         return res
 
@@ -44,7 +61,11 @@ class Anomalys:
 
         data = AnomalySerializer(anomalyObj).data
 
-        templateName = ANOMALY_DAILY_TEMPLATE if anomalyObj.anomalyDefinition.dataset.granularity == "day" else ANOMALY_HOURLY_TEMPLATE
+        templateName = (
+            ANOMALY_DAILY_TEMPLATE
+            if anomalyObj.anomalyDefinition.dataset.granularity == "day"
+            else ANOMALY_HOURLY_TEMPLATE
+        )
         cardTemplate = AnomalyCardTemplate.objects.get(templateName=templateName)
         data.update(data["data"]["anomalyLatest"])
 
@@ -55,16 +76,20 @@ class Anomalys:
         return res
 
     @staticmethod
-    def searchOnAnomalys(anomaliesObj,searchQuery):
+    def __searchOnAnomalys(anomaliesObj, searchQuery: str):
         """
-        Gets anomaly on user search 
+        Gets anomaly on user search
+        :param anomaliesObj: Objects of model Anomaly.Anomaly
+        :param searchQuery: string to be searched
         """
 
-        return anomaliesObj.filter(Q(anomalyDefinition__metric__icontains=searchQuery) 
-                                | Q(dimensionVal__icontains=searchQuery)
-                                | Q(anomalyDefinition__dataset__name__icontains=searchQuery) 
-                                | Q(anomalyDefinition__dataset__granularity__icontains=searchQuery) 
-                                | Q(anomalyDefinition__dimension__icontains=searchQuery))
+        return anomaliesObj.filter(
+            Q(anomalyDefinition__metric__icontains=searchQuery)
+            | Q(dimensionVal__icontains=searchQuery)
+            | Q(anomalyDefinition__dataset__name__icontains=searchQuery)
+            | Q(anomalyDefinition__dataset__granularity__icontains=searchQuery)
+            | Q(anomalyDefinition__dimension__icontains=searchQuery)
+        )
 
     @staticmethod
     def sortOnAnomalys(anomaliesObj, sorter):
@@ -72,19 +97,23 @@ class Anomalys:
         Sort anomaly on user input
         """
 
-        columnToSort = sorter.get("columnKey","")
+        columnToSort = sorter.get("columnKey", "")
         sortOrder = sorter.get("order", "")
 
         if columnToSort == "datasetName" and sortOrder == "ascend":
-            anomaliesObj = anomaliesObj.order_by("anomalyDefinition__dataset__name")        
+            anomaliesObj = anomaliesObj.order_by("anomalyDefinition__dataset__name")
         if columnToSort == "datasetName" and sortOrder == "descend":
             anomaliesObj = anomaliesObj.order_by("-anomalyDefinition__dataset__name")
-        
+
         if columnToSort == "granularity" and sortOrder == "ascend":
-            anomaliesObj = anomaliesObj.order_by("anomalyDefinition__dataset__granularity")
+            anomaliesObj = anomaliesObj.order_by(
+                "anomalyDefinition__dataset__granularity"
+            )
 
         if columnToSort == "granularity" and sortOrder == "descend":
-            anomaliesObj = anomaliesObj.order_by("-anomalyDefinition__dataset__granularity")
+            anomaliesObj = anomaliesObj.order_by(
+                "-anomalyDefinition__dataset__granularity"
+            )
 
         if columnToSort == "metric" and sortOrder == "ascend":
             anomaliesObj = anomaliesObj.order_by("anomalyDefinition__metric")
@@ -103,7 +132,7 @@ class Anomalys:
 
         if columnToSort == "contribution" and sortOrder == "descend":
             anomaliesObj = anomaliesObj.order_by("-data__contribution")
-        
+
         if columnToSort == "contribution" and sortOrder == "ascend":
             anomaliesObj = anomaliesObj.order_by("data__contribution")
 
@@ -123,7 +152,50 @@ class Anomalys:
             anomaliesObj = anomaliesObj.order_by("data__anomalyLatest__anomalyTime")
 
         return anomaliesObj
-        
 
+    @staticmethod
+    def createAnomaly(anomalyDef, dimVal: str, contriPercent: float, df):
+        """
+        Method to conduct the anomaly detection process
+        :param anomalyDef: object of model anomaly.anomalyDefinition
+        :param dimVal: dimension value
+        :param contriPercent: percentage contribution of given dimension values
+        :param df: dataframe with atleast timestamp & metric
+        :returns :
+        """
+        anomalyObj, _ = Anomaly.objects.get_or_create(
+            anomalyDefinition=anomalyDef, dimensionVal=dimVal, published=False
+        )
+        output = {"dimVal": dimVal}
+        try:
+            if dataFrameEmpty(df):
+                return
+            granularity = anomalyDef.dataset.granularity
+            result = detect(df, granularity)
+            result["contribution"] = contriPercent
+            result["anomalyLatest"]["contribution"] = contriPercent
+            timeThreshold = 3600 * 24 * 5 if granularity == "day" else 3600 * 24
+            toPublish = (
+                dt.datetime.now().timestamp()
+                - dp.parse(result["anomalyLatest"]["anomalyTimeISO"]).timestamp()
+                <= timeThreshold
+            )
+            if anomalyDef.highOrLow:
+                toPublish = (
+                    toPublish
+                    and anomalyDef.highOrLow.lower()
+                    == result["anomalyLatest"]["highOrLow"]
+                )
+            anomalyObj.data = result
+            anomalyObj.published = toPublish
+            anomalyObj.save()
+            output["published"] = toPublish
+            output["anomalyId"] = anomalyObj.id
+            output["success"] = True
+        except Exception as ex:
+            output["error"] = json.dumps(
+                {"message": str(ex), "stackTrace": traceback.format_exc()}
+            )
+            output["success"] = False
 
-
+        return output
--- api/ops/tasks/anomalyDetection.py
+++ api/ops/tasks/anomalyDetection.py
@@ -11,23 +11,26 @@ from anomaly.models import Anomaly, AnomalyDefinition, RunStatus, AnomalyCardTem
 from anomaly.serializers import AnomalySerializer
 from access.data import Data
 from access.utils import prepareAnomalyDataframes
-from ops.anomalyDetection import anomalyService
-from anomaly.services.slack import SlackAlert
 
 ANOMALY_DAILY_TEMPLATE = "Anomaly Daily Template"
-ANOMALY_HOURLY_TEMPLATE= "Anomaly Hourly Template"
+ANOMALY_HOURLY_TEMPLATE = "Anomaly Hourly Template"
 
 ANOMALY_DETECTION_RUNNING = "RUNNING"
 ANOMALY_DETECTION_SUCCESS = "SUCCESS"
 ANOMALY_DETECTION_ERROR = "ERROR"
+
 
 @shared_task
 def _anomalyDetectionSubTask(anomalyDef_id, dimVal, contriPercent, dfDict):
     """
     Internal anomaly detection subtask to be grouped by celery for each anomaly object
     """
+    from anomaly.services import Anomalys
+
     anomalyDefinition = AnomalyDefinition.objects.get(id=anomalyDef_id)
-    anomalyServiceResult = anomalyService(anomalyDefinition, dimVal, contriPercent, pd.DataFrame(dfDict))
+    anomalyServiceResult = Anomalys.createAnomaly(
+        anomalyDefinition, dimVal, contriPercent, pd.DataFrame(dfDict)
+    )
     return anomalyServiceResult
 
 
@@ -38,28 +41,55 @@ def anomalyDetectionJob(anomalyDef_id: int, manualRun: bool = False):
     :param anomalyDef_id: ID of the anomaly definition
     :param manualRun: Boolean determining whether task was manually initiated
     """
+
+    from anomaly.services.slack import SlackAlert
+
     runType = "Manual" if manualRun else "Scheduled"
     anomalyDefinition = AnomalyDefinition.objects.get(id=anomalyDef_id)
     anomalyDefinition.anomaly_set.update(published=False)
-    runStatusObj = RunStatus.objects.create(anomalyDefinition=anomalyDefinition, status=ANOMALY_DETECTION_RUNNING, runType=runType)
+    runStatusObj = RunStatus.objects.create(
+        anomalyDefinition=anomalyDefinition,
+        status=ANOMALY_DETECTION_RUNNING,
+        runType=runType,
+    )
     logs = {}
     allTasksSucceeded = False
     try:
         datasetDf = Data.fetchDatasetDataframe(anomalyDefinition.dataset)
-        dimValsData = prepareAnomalyDataframes(datasetDf, anomalyDefinition.dataset.timestampColumn, anomalyDefinition.metric, anomalyDefinition.dimension, anomalyDefinition.top)
+        dimValsData = prepareAnomalyDataframes(
+            datasetDf,
+            anomalyDefinition.dataset.timestampColumn,
+            anomalyDefinition.metric,
+            anomalyDefinition.dimension,
+            anomalyDefinition.top,
+        )
         detectionJobs = group(
-            _anomalyDetectionSubTask.s(anomalyDef_id, obj["dimVal"], obj["contriPercent"], obj["df"].to_dict("records")) for obj in dimValsData
+            _anomalyDetectionSubTask.s(
+                anomalyDef_id,
+                obj["dimVal"],
+                obj["contriPercent"],
+                obj["df"].to_dict("records"),
+            )
+            for obj in dimValsData
         )
         _detectionJobs = detectionJobs.apply_async()
         with allow_join_result():
             result = _detectionJobs.get()
-        Anomaly.objects.filter(id__in=[anomaly["anomalyId"] for anomaly in result if anomaly["success"]]).update(latestRun=runStatusObj)
-        logs["numAnomaliesPulished"] = len([anomaly for anomaly in result if anomaly.get("published")])
+        Anomaly.objects.filter(
+            id__in=[anomaly["anomalyId"] for anomaly in result if anomaly["success"]]
+        ).update(latestRun=runStatusObj)
+        logs["numAnomaliesPulished"] = len(
+            [anomaly for anomaly in result if anomaly.get("published")]
+        )
         logs["numAnomalySubtasks"] = len(_detectionJobs)
-        logs["log"] = json.dumps({detection.id: detection.result for detection in _detectionJobs})
+        logs["log"] = json.dumps(
+            {detection.id: detection.result for detection in _detectionJobs}
+        )
         allTasksSucceeded = all([anomalyTask["success"] for anomalyTask in result])
     except Exception as ex:
-        logs["log"] = json.dumps({"stackTrace": traceback.format_exc(), "message": str(ex)})
+        logs["log"] = json.dumps(
+            {"stackTrace": traceback.format_exc(), "message": str(ex)}
+        )
         runStatusObj.status = ANOMALY_DETECTION_ERROR
     else:
         runStatusObj.status = ANOMALY_DETECTION_SUCCESS
@@ -74,23 +104,47 @@ def anomalyDetectionJob(anomalyDef_id: int, manualRun: bool = False):
     if runStatusObj.status == ANOMALY_DETECTION_SUCCESS:
         if logs.get("numAnomaliesPulished", 0) > 0:
             message = f"{logs['numAnomaliesPulished']} anomalies published. \n"
-            message = message + f"Anomaly Definition: {anomalyDefinition.metric} {anomalyDefinition.dimension} {anomalyDefinition.highOrLow} \n"
-            message = message + f"Dataset: {anomalyDefinition.dataset.name} | Granularity: {anomalyDefinition.dataset.granularity} \n \n"
-            
-            highestContriAnomaly = anomalyDefinition.anomaly_set.order_by("data__contribution").last()
+            message = (
+                message
+                + f"Anomaly Definition: {anomalyDefinition.metric} {anomalyDefinition.dimension} {anomalyDefinition.highOrLow} \n"
+            )
+            message = (
+                message
+                + f"Dataset: {anomalyDefinition.dataset.name} | Granularity: {anomalyDefinition.dataset.granularity} \n \n"
+            )
+
+            highestContriAnomaly = anomalyDefinition.anomaly_set.order_by(
+                "data__contribution"
+            ).last()
             data = AnomalySerializer(highestContriAnomaly).data
-            templateName = ANOMALY_DAILY_TEMPLATE if anomalyDefinition.dataset.granularity == "day" else ANOMALY_HOURLY_TEMPLATE
+            templateName = (
+                ANOMALY_DAILY_TEMPLATE
+                if anomalyDefinition.dataset.granularity == "day"
+                else ANOMALY_HOURLY_TEMPLATE
+            )
             cardTemplate = AnomalyCardTemplate.objects.get(templateName=templateName)
             data.update(data["data"]["anomalyLatest"])
-            
-            message = message + html2text.html2text(Template(cardTemplate.title).render(Context(data))) + "\n"
-            message = message + html2text.html2text(Template(cardTemplate.bodyText).render(Context(data)))
+
+            message = (
+                message
+                + html2text.html2text(
+                    Template(cardTemplate.title).render(Context(data))
+                )
+                + "\n"
+            )
+            message = message + html2text.html2text(
+                Template(cardTemplate.bodyText).render(Context(data))
+            )
 
             name = "anomalyAlert"
             SlackAlert.slackAlertHelper(title, message, name)
-    
+
     if runStatusObj.status == ANOMALY_DETECTION_ERROR:
-        message = "Anomaly Detection Job failed on AnomalyDefintion id : " + str(anomalyDef_id) + "\n"
+        message = (
+            "Anomaly Detection Job failed on AnomalyDefintion id : "
+            + str(anomalyDef_id)
+            + "\n"
+        )
         message = message + str(logs["log"])
         name = "appAlert"
         SlackAlert.slackAlertHelper(title, message, name)
