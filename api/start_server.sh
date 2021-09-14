dcbc3bfb7fd48a84b69bd34e398ed3172586f447
ankit <ankitkpandey1729@gmail.com>
2021-09-14 17:49:44 +0530

    Celery routes updated

--- api/start_server.sh
+++ api/start_server.sh
@@ -15,7 +15,8 @@ mv static_root/* /home/staticfiles
 rm -rf static_root/
 (/usr/bin/redis-server) &
 (gunicorn app.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3 --timeout 300) &
-(celery -A app worker --concurrency=6 -l INFO --purge) &
+(celery -A app worker --concurrency=2 -l INFO --purge) &
+(celery -A app worker --concurrency=4 -Q anomalySubTask -l INFO --purge) &
 (celery -A app beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler) &
 echo "starting nginx"
 nginx -g "daemon off;"
