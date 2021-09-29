6bf8fcd33adb7bd223a4b26437e807ef64898628
Kshitij Mittal <kshitij.mittal01@gmail.com>
2021-09-29 10:54:08 +0530

    alert micro-service

---
+++ alerts-api/run.py
@@ -0,0 +1,4 @@
+from src.app import app
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8100)
--- api/anomaly/services/alerts.py
+++ api/anomaly/services/alerts.py
@@ -1,19 +1,21 @@
-
 import logging
-from django.core.mail import EmailMultiAlternatives
+import os
 from email.mime.image import MIMEImage
-from django.conf import settings
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-from anomaly.services.settings import ANOMALY_ALERT_SLACK_ID, APP_ALERTS_SLACK_ID, SLACK_BOT_TOKEN, SEND_EMAIL_TO
+
+import requests
 from anomaly.models import Setting
 from anomaly.services.plotChart import PlotChartService
+from anomaly.services.settings import ANOMALY_ALERT_SLACK_ID, APP_ALERTS_SLACK_ID, SEND_EMAIL_TO, SLACK_BOT_TOKEN
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 
 logger = logging.getLogger(__name__)
+ALERT_API_URL = os.environ.get("ALERT_API_URL", "localhost:8100")
+
 
 class SlackAlert:
 
-    def slackAlertHelper(title, message, name, details="", anomalyId: int = None ):
+    def slackAlertHelper(title, message, name, details="", anomalyId: int = None):
         """
         Helper method for slackAlert
         """
@@ -31,59 +33,34 @@ class SlackAlert:
                     token = setting["value"]
             # Anomaly Detection Alert 
             if name == "anomalyAlert":
-                SlackAlert.cueObserveAnomalyAlert(token, anomalyAlertChannelId, anomalyId , title, message, details)
+                url = f'{ALERT_API_URL}/alerts/anamoly-alert'
+                payload = {
+                    "token": token,
+                    "anomalyAlertChannelId": anomalyAlertChannelId,
+                    "title": title,
+                    "meesage": message,
+                    "details": details,
+                    "fileImg": PlotChartService.anomalyChartToImgStr(anomalyId)
+                }
+                requests.request("POST", url, data=payload)
             # AppAlert
             if name == "appAlert":
-                SlackAlert.cueObserveAlert(token, appAlertChannelId, title, message)
+                url = f'{ALERT_API_URL}/alerts/app-alert'
+                payload = {
+                    "token": token,
+                    "appAlertChannelId": appAlertChannelId,
+                    "title": title,
+                    "meesage": message
+                }
+                requests.request("POST", url, data=payload)
 
         except Exception as ex:
             logger.error("Slack URL not given or wrong URL given:%s", str(ex))
 
 
-    def cueObserveAnomalyAlert(token, channelId, anomalyId, title="", message="", details=""):
-        """
-        Image uploads in slack
-        """
-        fileImg = PlotChartService.anomalyChartToImgStr(anomalyId)
-        client = WebClient(token=token)  
-        # The name of the file you're going to upload
-        fileName = fileImg
-        try:
-            # Call the files.upload method using the WebClient
-            # Uploading files requires the `files:write` scope
-            result = client.files_upload(
-            # ID of channel that you want to upload file to
-                channels=channelId,
-                # initial_comment="Here's my file :smile:",
-                initial_comment = message + "\n" + details ,
-                title=title,
-                file=fileName,
-            )
-            # Log the result
-            logger.info(result)
-
-        except SlackApiError as e:
-            logger.error("Error uploading file: {}".format(e))
-
-    def cueObserveAlert(token, channelId, title="", message=""):
-        """ Post message in slack"""
-
-        client = WebClient(token=token)
-        try:
-            # Call the chat.postMessage method using the WebClient
-            result = client.chat_postMessage(
-                channel=channelId, 
-                text= "*" + title + "*" + "\n" + message 
-            )
-            logger.info(result)
-
-        except SlackApiError as e:
-            logger.error(f"Error posting message: {e}")
-
-
 class EmailAlert:
 
-    def sendEmail( message, details, subject,anomalyId ):
+    def sendEmail(message, details, subject, anomalyId):
         """
         Email alert with image
         """
@@ -97,7 +74,7 @@ class EmailAlert:
             logger.info("Sending email procedure starts")
             imgByte = PlotChartService.anomalyChartToImgStr(anomalyId)
             subject, from_email, to = subject, settings.EMAIL_HOST_USER, sendEmailTo
-            body_html =  message + details + '''
+            body_html = message + details + '''
                 <html>
                     <body>
                         <img src="cid:logo.png" alt="anomaly image" />
