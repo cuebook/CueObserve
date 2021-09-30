83c18608a0e08203f6c7c380e0800c9f74cffb64
vincue <vineet@cuebook.ai>
2021-09-30 17:40:47 +0530

    Bug fix

--- search-api/search/__init__.py
+++ search-api/search/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from .cardTemplates import views, models  # pylint: disable=W0404
-from .globalDimensions import views, models  # pylint: disable=W0404
-
 app = Flask(__name__)
 
 # Configurations
@@ -15,6 +12,9 @@ db = SQLAlchemy(app)
 migrate = Migrate()
 migrate.init_app(app, db)
 
+
+# from .cardTemplates import views, models  # pylint: disable=W0404
+from .globalDimensions import views, models  # pylint: disable=W0404
 
 # if __name__ == "app":
 #     app = create_app(env, args)
