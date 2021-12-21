0dc910648dfcc6bc55e7ef5dbc13cdde4bcb197a
praveencuebok <praveen@cuebook.ai>
2021-12-21 19:13:24 +0530

    firebasePythonSdk Google analytics is added for server-side monitoring, we are not moving with firebase on server side monitoring

--- api/start_server_new.sh
+++ api/start_server_new.sh
@@ -15,6 +15,6 @@ rm -rf static_root/
 (celery -A app worker --concurrency=2 -l INFO --purge) &
 (celery -A app worker --concurrency=4 -Q anomalySubTask -l INFO --purge) &
 (celery -A app beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler) &
-python firebase.py &
+python telemetry.py &
 
 gunicorn app.wsgi --reload --user www-data --bind 0.0.0.0:8000 --workers 3 --timeout 300
